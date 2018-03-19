b7d9c6ec7cc5814c02f1c00acd0fe678f3e557ae
Sean Taylor <sean666888@users.noreply.github.com>
2018-03-20 11:50:20 +1300

    Update startup.sh

--- include/startup.sh
+++ include/startup.sh
@@ -12,10 +12,23 @@ if [ ! -f $CCACHE_DIR/ccache.conf ]; then
 	echo "Initializing ccache in $CCACHE_DIR..."
 	ccache -M 50G
 fi
-
-export USE_CCACHE=1
+# Installing 'repo'
+mkdir ~/bin
+echo "PATH=~/bin:$PATH" >> ~/.bashrc
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+. ~/.bashrc
+export USE_CCACHE=0
 export CCACHE_COMPRESS=1
 export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"
-
+sudo apt-get install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip
+mkdir LineageOS
+cd LineageOS/
+git config --global user.name "Sean Taylor"
+git config --global user.email "sean.r.taylor.1912@gmail.com"
+repo init -u git://github.com/LineageOS/android.git -b lineage-15.1
+repo sync
+source build/envsetup.sh
+brunch lineage_a3xeltexx-userdebug
 # Show general info
 screenfetch
